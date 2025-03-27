b32d20e3100412b570cae02eb52d094c6178d156
ApekshaV1 <dasapeksha5@gmail.com>
2025-03-27 09:38:44 +0530

    Changes to be committed: 	modified:   package-lock.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "C:/Vedashat/AV's Docs/Projects/Elixir_The blood donors app/src/App.js";
-import "C:/Vedashat/AV's Docs/Projects/Elixir_The blood donors app/src/styles.css"; // Import global styles
+import App from "../src/App.js";
+import "../src/styles.css"; // Import global styles
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
